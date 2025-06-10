348e4f3f4b7c9d0494388b1b655b1992fb466266
Jesse Qin <jesseqin.office@gmail.com>
2025-06-10 13:53:59 -0400

    Convert to Jesse's website

--- site.config.ts
+++ site.config.ts
@@ -2,24 +2,24 @@ import { siteConfig } from './lib/site-config'
 
 export default siteConfig({
   // the site's root Notion page (required)
-  rootNotionPageId: '7875426197cf461698809def95960ebf',
+  rootNotionPageId: '1cad70bfc05f802a8519df6546101d17',
 
   // if you want to restrict pages to a single notion workspace (optional)
   // (this should be a Notion ID; see the docs for how to extract this)
   rootNotionSpaceId: null,
 
   // basic site info (required)
-  name: 'Next.js Notion Starter Kit',
+  name: 'Jesse Qin\'s Blogs',
   domain: 'nextjs-notion-starter-kit.transitivebullsh.it',
-  author: 'Travis Fischer',
+  author: 'Jesse Qin',
 
   // open graph metadata (optional)
-  description: 'Example Next.js Notion Starter Kit Site',
+  description: 'Welcome to my blogs!',
 
   // social usernames (optional)
-  twitter: 'transitive_bs',
-  github: 'transitive-bullshit',
-  linkedin: 'fisch2',
+  twitter: 'Jesse8868',
+  github: 'JesseQin123',
+  linkedin: 'koodall-jesseqin-phd',
   // mastodon: '#', // optional mastodon profile URL, provides link verification
   // newsletter: '#', // optional newsletter URL
   // youtube: '#', // optional youtube channel name or `channel/UCGbXXXXXXXXXXXXXXXXXXXXXX`
@@ -50,16 +50,28 @@ export default siteConfig({
 
   // whether to use the default notion navigation style or a custom one with links to
   // important pages. To use `navigationLinks`, set `navigationStyle` to `custom`.
-  navigationStyle: 'default'
-  // navigationStyle: 'custom',
-  // navigationLinks: [
-  //   {
-  //     title: 'About',
-  //     pageId: 'f1199d37579b41cbabfc0b5174f4256a'
-  //   },
-  //   {
-  //     title: 'Contact',
-  //     pageId: '6a29ebcb935a4f0689fe661ab5f3b8d1'
-  //   }
-  // ]
+  // navigationStyle: 'default'
+  navigationStyle: 'custom',
+  navigationLinks: [
+    {
+      title: 'Blogs',
+      pageId: '1cad70bfc05f8151bf44ddc6991d2d57'
+    },
+    {
+      title: 'Projects',
+      pageId: '1d2d70bfc05f8067abbae56b37288336'
+    },
+    {
+      title: 'Readings',
+      pageId: '1cad70bfc05f81faa2d9f743496d3801'
+    },
+    {
+      title: 'Skills',
+      pageId: '1d2d70bfc05f802b8fb9dfb8998d70ac'
+    },
+    {
+      title: 'About',
+      pageId: '1cad70bfc05f8078a0d9d049ca20bdcf'
+    }
+  ]
 })
